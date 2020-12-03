ce7e871606cb102649bbdee14b8f7cb62e2ae0e6
Manish Rajak <manish.rajak2055@gmail.com>
2020-12-03 18:07:23 +0545

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const pageNotFoundHandler = require('./controllers/404Error');
 const app = express();
 
 app.set('view engine', 'ejs'); // sets the method of rendering to the ejs 
@@ -19,8 +19,6 @@ app.use(shopRoutes); // shop routes
 
 
 // if no routes are found then render the 404 page not found page 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(pageNotFoundHandler.get404);
 
 app.listen(3000); // listening to the port 3000 -- Production environment change these
--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 
 exports.getAddProduct = (req, res, next) => {
@@ -12,12 +12,15 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+  // Logic to add the product to the database through the model
+  const product = new Product(req.body.title);
+  product.save();
     res.redirect('/');
 }
 
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
