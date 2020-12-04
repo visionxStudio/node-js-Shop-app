5abba421b959116e28a375115c245f521fafbd4e
Manish Rajak <manish.rajak2055@gmail.com>
2020-12-04 16:59:54 +0545

    first commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,8 +11,23 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
+  // extract data from the form 
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
   // Logic to add the product to the database through the model
-  const product = new Product(req.body.title);
+  const product = new Product(title, imageUrl, description, price);
   product.save();
     res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+ const products = Product.fetchAll()
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    })
+
 }
--- models/product.js
+++ models/product.js
@@ -1,12 +1,15 @@
 const products = [];
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
-        products.push(this);
+        products.push(this); // this will push all the elements that are in constructor to the products object
     }
 
     static fetchAll() {
