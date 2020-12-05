0ce0a47b734f2e8cfdc3d7d67a2dfc52da23c624
Manish Rajak <manish.rajak2055@gmail.com>
2020-12-05 20:00:27 +0545

    first commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,12 +10,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct = (req, res, next) => {
+exports.getProductDetail = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    console.log(product);
+    res.render('shop/product-detail', {
+      pageTitle: product.title,
+      product: product,
+      path: '/product-details',
+    });
   });
-  res.redirect('/');
+  // res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
